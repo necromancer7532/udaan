965b14a9b6fa442c6b2f6bd3ea199fde7158aca6
Vishnu Remesh <vishnu.ramesh780@gmail.com>
2018-03-07 15:59:21 +0530

    code
    first commit

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+The index.html file has to be opened using a browser but since its a Php backend an apache server has to be running
+I used and recommend using xampp and putting the entire folder in htdocs and then going to localhost to find the index page
+ 
---
+++ finder.php
@@ -0,0 +1,21 @@
+<?php
+$str=$_POST["data_text"];
+ini_set("allow_url_fopen", 1);
+$json = file_get_contents('https://newsapi.org/v2/top-headlines?apiKey=cd1db87dec794c2288c915ba6abeee94&country=in');
+$obj = json_decode($json);
+$n=strlen($str);
+foreach ($obj->articles as $objects)
+	{	
+		{
+			$n1=strlen($objects->title);
+			for ($x = 0; $x <= $n1; $x++)
+			{
+				if(strcmp(substr($objects->title,$x,$n),$str)==0)
+					{echo $objects->title;
+						echo "\n";
+					}
+			}
+		}
+	}
+
+?>
